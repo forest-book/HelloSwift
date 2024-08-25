d28a082f1e43247e601296c304cf481e55c53d2f
forest-book <124020491+forest-book@users.noreply.github.com>
2024-08-25 17:34:46 +0900

    init add .gitignore

---
+++ helloswift/helloswift/helloswiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  helloswiftApp.swift
+//  helloswift
+//
+//  Created by user on 2024/08/25.
+//
+
+import SwiftUI
+
+@main
+struct helloswiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
